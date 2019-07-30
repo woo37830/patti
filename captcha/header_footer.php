4f5585562a8caa8e3e2fe37dc7482ffa4bb81d89
John Wooten <woo@jwooten37830.com>
2019-07-30 11:49:43 -0400

    Fixed a parse error in header_footer.php

--- captcha/header_footer.php
+++ captcha/header_footer.php
@@ -1,22 +1,23 @@
 <?php
 function page_header()
 {
-  $script = <<< 'EOS'
- <head>
- <link rel="stylesheet" type="text/css" href="https://jwooten37830.com/patti/styles/example.css">
- </head><body>
- <div id="content">
- <center>
- <img src="https://jwooten37830.com/patti/images/green_logo.gif" style="width:100px;height:100px">
- <br /><br /><br />
- </center>
- EOS;
-   return $script;
+  $script = "<head>" . 
+  "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://jwooten37830.com/patti/styles/example.css\"/>" .
+ "</head><body>" .
+ "<div id=\"content\">" .
+ "<center>" .
+ "<img src=\"https://jwooten37830.com/patti/images/green_logo.gif\" style=\"width:100px;height:100px\"/>" .
+ "<br /><br /><br />" .
+ "</center>";
+ return $script;
 }
 
 function git_footer() {
   $rev = exec('git rev-parse --short HEAD');
   $branch = exec('git rev-parse --abbrev-ref HEAD');
-  return "<footer><hr />Commit: $rev - Branch: $branch</footer>";
-}
+  $gf =  "<footer><hr />Commit: " . $rev . " - Branch: " . $branch . "</footer>";
+ 
+  return $gf;
+ }
 ?>
+
