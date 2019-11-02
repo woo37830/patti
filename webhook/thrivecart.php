d152f26845d289a0f9c65d496dcc0cbca1b09a81
woo37830 <jwooten37830@me.com>
2019-11-02 11:18:11 -0400

    Starting refactor of thrivecart.php

--- webhook/thrivecart.php
+++ webhook/thrivecart.php
@@ -1,6 +1,6 @@
 <?php
 //
-
+require('post_api_url');
 /**
  * AllClients Account ID and API Key.
  */
@@ -157,57 +157,6 @@ fwrite($fh,"\nThis email '%s' can be added to the $_SESSION and saved in databas
 
 } else {
   fwrite($fh,"\nError: Invalid Product information, product_name='%s' and item_id='%s'\n",$product_name, $item_id);
-}
-/**
- * Post data to URL with cURL and return result XML string.
- *
- * Outputs cURL error and exits on failure.
- *
- * @param string $url
- * @param array  $data
- *
- * @return string
- */
-function post_api_url($url, array $data = array()) {
-	global $nl;
-
-	// Initialize a new cURL resource and set the URL.
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-
-	// Form data must be transformed from an array into a query string.
-	$data_query = http_build_query($data);
-
-	// Set request type to POST and set the data to post.
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_query);
-
-	// Set cURL to error on failed response codes from AllClients server,
-	// such as 404 and 500.
-	curl_setopt($ch, CURLOPT_FAILONERROR, true);
-
-	// Set cURL option to return the response from the AllClients server,
-	// otherwise $output below will just return true/false.
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-	// Post data to API.
-	$output = curl_exec($ch);
-
-	// Exit on cURL error.
-	if ($output === false) {
-		// It is important to close the cURL session after curl_error()
-		fwrite($fh,"\ncURL returned an error: %s\n", curl_error($ch));
-		curl_close($ch);
-    fclose($fh);
-    http_response_code(400);
-    exit;
-	}
-
-	// Close the cURL session
-	curl_close($ch);
-
-	// Return response
-	return $output;
 }
 
 function pretty_dump($mixed = null) {
