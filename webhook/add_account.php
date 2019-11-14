5212cf21bbb7bb940f551593caead37d576231e2
John Wooten <woo@jwooten37830.com>
2019-11-14 12:59:45 -0500

    Corrected xml parsing error and cast

--- webhook/add_account.php
+++ webhook/add_account.php
@@ -17,12 +17,13 @@ $data = array(
         'group' => $group_name,
 );
 fwrite($fh,"data array established\n");
-$results_xml = thrivecart_api($url, $data, $fh);
+$results_xml = thrivecart_api($url, $data, $fh); // returns simplexml_load_string object representation
 if ($results_xml === false) {
 	logit($results_xml, "\nError parsing XML", "failure");
   http_response_code(400);
 	exit;
 }
+fwrite($fh,"results_xml = $results_xml->message\n");
 /**
  * If an API error has occurred, the results object will contain a child 'error'
  * SimpleXMLElement parsed from the error response:
@@ -32,9 +33,9 @@ if ($results_xml === false) {
  *     <error>Authentication failed</error>
  *   </results>
  */
- $xml = simplexml_load_string($results_xml);
 
 if (isset($results_xml->error)) {
+  fwrite($fh,"An error occurred $fesults_xml->error\n");
   logit($data['email'],$results_xml->error, "failure" );
   http_response_code(400);
   exit;
@@ -43,8 +44,9 @@ if (isset($results_xml->error)) {
  * If no error was returned, the AddContact results object will contain a
  * 'contactid' child SimpleXMLElement, which can be cast to an integer.
  */
- $account_id = (string)$xml->account_id;
+ $account_id = (int)$results_xml->accountid;
 
+fwrite($fh,"account_id is '$account_id'\n");
 
 // Here I write the account information using addUser in mysql_common.php
 addUser($data['email'],   $thrivecartid, $account_id);
