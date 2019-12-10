1973d7e36b478b141804f72244d5ef3336516713
woo37830 <jwooten37830@mac.com>
2019-12-10 14:10:00 -0500

    Added response.txt

--- abandon.php
+++ abandon.php
@@ -4,5 +4,6 @@ $date = (new DateTime('NOW'))->format("y:m:d h:i:s");
 if( $fh = fopen($myFile, 'a') ) {
 fwrite($fh, "\n-----------------".$date."-----------------------------------\n");
 }
+http_response_code(200);
 fwrite($fh,"request:".json_encode($_REQUEST)."\n");
 ?>
