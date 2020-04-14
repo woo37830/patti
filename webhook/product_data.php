dd673938f1123a544f6c8c9017a13c2eb3eeadb7
woo37830 <jwooten37830@mac.com>
2020-04-14 22:58:21 +0000

    Corrected group for product-10

--- webhook/product_data.php
+++ webhook/product_data.php
@@ -3,5 +3,5 @@ $products = array( "product-9" => "RE - BUZZ ($69)", "product-12" => "RE - IMPAC
                    "product-13" => "RE - IMPACT ($99)", "product-14" => "RE - IMPACT ($99)",
                    "product-15" => "RE - IMPACT ($99)", "product-16" => "RE - IMPACT ($99)",
                    "product-17" => "RE - IMPACT ($99)",
-	           "product-19" => "$75/Jason Holdinski Hyper Farmer - EngageMore Impact Level");
+	           "product-19" => "RE - IMPACT ($99)");
 ?>
