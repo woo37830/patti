e549271727a71843c7524232badaa41f8760e8cc
woo37830 <jwooten37830@mac.com>
2020-04-20 21:55:56 +0000

    Added product-22

--- webhook/product_data.php
+++ webhook/product_data.php
@@ -4,5 +4,6 @@ $products = array( "product-9" => "RE - BUZZ ($69)", "product-12" => "RE - IMPAC
                    "product-15" => "RE - IMPACT ($99)", "product-16" => "RE - IMPACT ($99)",
                    "product-17" => "RE - IMPACT ($99)",
 	           "product-19" => "RE - IMPACT ($99)",
-	           "product-21" => "RE - IMPACT ($99)");
+	           "product-21" => "RE - IMPACT ($99)",
+                   "product-22" => "RE - BUZZ ($69)");
 ?>
