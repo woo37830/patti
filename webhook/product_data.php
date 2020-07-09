c70b042c4916dfcd2d9cde312302dac375e0d0d4
woo37830 <jwooten37830@me.com>
2020-07-09 16:12:46 -0400

    Fix for product-29

--- webhook/product_data.php
+++ webhook/product_data.php
@@ -10,5 +10,5 @@ $products = array( "product-9" => "RE - BUZZ ($69)",
 	           "product-21" => "RE - IMPACT ($99)",
                    "product-22" => "RE - BUZZ ($69)",
                    "product-24" => "RE - 14 DAY TREE TRIAL",
-                   "product-29" => "RE - 14 DAY TREE TRIAL");
+                   "product-29" => "RE - 14 DAY FREE TRIAL");
 ?>
