70b09c25339cfe2f7cd4a0a1a5966e57ca294731
woo37830 <jwooten37830@me.com>
2020-08-03 17:12:20 -0400

    Developing tool to check consistency between users and crm

--- webhook/get_all_accounts.php
+++ webhook/get_all_accounts.php
@@ -1,21 +1,21 @@
 <?php
-function getAccounts($today="", $email="")
+function getAllAccounts()
 {
-  require '../webhook/config.ini.php';
-  require_once '../webhook/thrivecart_api.php';
+  require 'config.ini.php';
+  require 'thrivecart_api.php';
 
-  require_once '../webhook/mysql_common.php';
-  require_once '../webhook/utilities.php';
+  require 'mysql_common.php';
+  require 'utilities.php';
   $account_id   = $config['MSG_USER'];
   $api_key      = $config['MSG_PASSWORD'];
   $api_endpoint = 'https://secure.engagemorecrm.com/api/2/';
 
   $url = $api_endpoint . 'GetAccounts.aspx';
 
-
   $data = array(
   	'apiusername' => $account_id,
-  	'apipassword'    => $api_key
+  	'apipassword'    => $api_key,
+    'showpendingdelete' => 1
   );
   $results_xml = thrivecart_api($url, $data); // returns simplexml_load_string object representation
 
@@ -30,16 +30,15 @@ function getAccounts($today="", $email="")
    */
 
   if (isset($results_xml->error)) {
-  //  echo "\nresults_xml: " . $results_xml . "\n";
-    echo "\nFailure: " . $results_xml->error . "\n";
-    logit($email,$results_xml->error, "FAILURE: get_accounts.php: $results_xml->error" );
-    die ("FAILURE: $results_xml->error");
+    echo "\nFailure: " . $results_xml->error . "<br />\n";
+    return $results_xml->error;
   }
   var_dump($results_xml);
   $accounts = array();
   foreach($results_xml->accounts as $account){
-      array_push($accounts, $account);
+    array_push($accounts, $account);
   }
+
   return $accounts;
 
 }
