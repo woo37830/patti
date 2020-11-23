86acc837bf7b18fa1776bb48d9a56a9d5367d4a2
woo37830 <jwooten37830@me.com>
2020-11-23 10:44:44 -0500

    Fixed index.php to use datatables and match style

--- web/index.php
+++ web/index.php
@@ -25,9 +25,30 @@ require('fancyAuthentication.php');
 	  <link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/themes/color.css">
 	  <link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/demo/demo.css">
   <script type="text/javascript" class="init">
+    var dataSet = [
+    [ "<a href=\"../index.html\">Documentation</a>",
+      "System Architect", "Update"],
+    ["<a href=\"./monthly_report.php\">Monthly Report</a>",
+      "Get a monthly report",""],
+    ["<a href=\"./getAccountId.php\">Get Account Id</a>",
+      "Get the account id for an email",""],
+    ["<a href=\"./maintain_accounts.php\">Maintain Accounts</a>",
+      "Maintain Engagemore Accounts",""],
+    ["<a href=\"./maintain_users.php\">Maintain Users</a>","",""],
+    ["<a href=\"./Consistency.php\">Consistency Report</a>","",""]
+    ];
+
      var oTable;
      var json;
      $(document).ready(function() {
+       $('#index').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "Application" },
+            { title: "Purpose" },
+            { title: "Comment" }
+        ]
+    } );
        $.ajax({
       url: "./git-info.php",
       dataType: "text",
@@ -47,29 +68,12 @@ require('fancyAuthentication.php');
     <h1>Applications</h1>
     <hr />
     <br />
-  <table id="users" class="tablesorter">
-  <thead>
-  <tr><th>Application</th><th>Purpose</th><th>Comment</th></tr>
-  </thead>
-  <tbody>
-  <tr><td><a href="../index.html">Documentation</a></td><td>Documents describing applications</td><td>Update theese</th></tr>
-  <tr><td><a href="./monthly_report.php">Monthly Report</a></td><td>Get a monthly report</td><td>Add input for month</th></tr>
-  <tr><td><a href="../billing.php">Billing Report</a></td><td>Get a report on Billing</td><td>Add input for month</th></tr>
-  <tr><td><a href="./getAccountId.php">Get Account Id</a></td><td>Get the account id for an email</td><td></td></tr>
-  <tr><td><a href="./maintain_accounts.php">Maintain Accounts</a></td><td>Maintain Engagemore Accounts</td><td></td></tr>
-  <tr><td><a href="./maintain_users.php">Maintain Users</a></td><td>List, Update, delete users_db.users</td><td>Add DataTables and nav at top and new user</td></tr>
-  <tr><td><a href="./Consistency.php">Consistency Report</a></td><td>Check for extra or missing account information,csv files to tmp</td><td>Add check for order id, etc., Add links to Update User</td></tr>
-  <tr><td><a href="./testAddContact.php">Add Contact</a></td><td>Add a contact to an account</td><td>Style info?</td></tr>
-  <tr><td>Add Contact Note</td><td>Create a note and add it to a contact</td><td>Working with login</td></tr>
-  <tr><td>GetContact</td><td>Get a contact using their email</td><td>Add login</td></tr>
-  <tr><td>Get Contacts</td><td>Get the list of contacts for an account</td><td>Add login and links to Add Contact Note</td></tr>
-  </tbody>
-  </table>
+    <table id="index" class="tablesorter" width="95%"></table>
   </center>
 </div> <!-- end of page -->
 </div> <!-- end of content div -->
-</div>
+</div> <!-- end of wrapper -->
 <footer class="footer" id="footer-div" /footer>
-</div>
+
 </body>
 </html>
