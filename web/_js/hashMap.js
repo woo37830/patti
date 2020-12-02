f8f756db7875b76a5a87e03e620a48e9e25f1cc8
woo37830 <jwooten37830@me.com>
2020-12-02 10:36:27 -0500

    formatting

--- web/_js/hashMap.js
+++ web/_js/hashMap.js
@@ -1,6 +1,6 @@
 function HashMap() {
   var obj = [];
-  function find(key){
+  function find(key) {
     var i = obj.length;
     while (i--) {
       var curr = obj[i];
@@ -13,31 +13,30 @@ function HashMap() {
   var d = function dictionary(key, value) {
     var index = find(key);
     if (value) {
-      if (index !== null){
+      if (index !== null) {
         obj.splice(index, 1);
       }
       obj.push([key, value]);
- 
     } else {
-      if (index !== null){
+      if (index !== null) {
         return obj[index][1];
       }
     }
-  }
-  d.get = function(key) {
-      return obj[find(key)][1];
-  }
-  d.size = function(){
+  };
+  d.get = function (key) {
+    return obj[find(key)][1];
+  };
+  d.size = function () {
     return obj.length;
-  }
-  d.delete = function(key) {
+  };
+  d.delete = function (key) {
     obj.splice(find(key), 1);
-  }
-  d.each = function(func){
-    for (var i = 0; i<obj.length; i++){
-      var item = obj[i]
-      func(item[0], item[1]); 
+  };
+  d.each = function (func) {
+    for (var i = 0; i < obj.length; i++) {
+      var item = obj[i];
+      func(item[0], item[1]);
     }
-  }
+  };
   return d;
 }
