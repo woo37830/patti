b90c6cec4cf7d4ac0ab10a88239fbb0e89db7e11
woo37830 <jwooten37830@me.com>
2022-01-04 09:08:33 -0500

    Change facebook.php to match Safari instructions

--- webhook/facebook.php
+++ webhook/facebook.php
@@ -1,30 +1,8 @@
 <?php
 
 $verify_token = 'meatyhamhock';
-$json_data = json_encode($_REQUEST);
-if( $verify_token != $_REQUEST['hub_verify_token'] ) {
-	die("Invalid verify token");
+$challenge = $_REQUEST['hub_challenge'];
+if( $verify_token === $_REQUEST['hub_verify_token'] ) {
+   echo $challenge;
 }
 ?>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset="UTR-8">
-<script type="text/javascript" class="init">
-const params = new URL(location.href).searchParams;
-let challenge=params.get("hub.challenge");
-document.write(challenge);
-let client=params.get("clientId");
-document.write(client);
-let name=params.get("name");
-document.write(name);
-let email=params.get("email");
-document.write(email);
-let form=params.get("formId");
-document.write(form);
-</script>
-</head>
-<body>
-
-
-</body>
-</html>
