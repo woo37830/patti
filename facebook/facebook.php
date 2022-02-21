cac3efe3b35902b7111922ed1cebc02b1f173083
woo37830 <jwooten37830@me.com>
2022-02-21 16:51:53 -0500

    Work starting on facebook webhook for AllClients

--- facebook/facebook.php
+++ facebook/facebook.php
@@ -5,4 +5,6 @@ $challenge = $_REQUEST['hub_challenge'];
 if( $verify_token === $_REQUEST['hub_verify_token'] ) {
    echo $challenge;
 }
+$input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($input, true))
 ?>
