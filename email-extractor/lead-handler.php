d8b937f025add2842e4950e38d974c95fc310c74
woo37830 <jwooten37830@me.com>
2022-08-07 15:18:31 -0400

    More checks

--- email-extractor/lead-handler.php
+++ email-extractor/lead-handler.php
@@ -38,17 +38,23 @@ require_once 'Prospect.php';
 // attachments are saved.  If someone emails you maliciousFile.php and can access the file, it is bad.
 
 $saveAttachments = 1;
+$attachmentLog = "log";
+// Set to TRUE for testing
+$useTestEmail = FALSE;
+$error = 0;
 
 // strip slashes away if magic_quotes_gpc is on
-if(get_magic_quotes_gpc() == 1)
-	{
-		$postedEmail = stripslashes($_POST['email']);
-	}
-else
-	{
-		$postedEmail = $_POST['email'];
-	}
-
+if( $useTestEmail != TRUE )
+{
+	if(get_magic_quotes_gpc() == 1)
+		{
+			$postedEmail = stripslashes($_POST['email']);
+		}
+	else
+		{
+			$postedEmail = $_POST['email'];
+		}
+}
 ######################################################
 ##
 ## 	JSON FORMATTED EMAIL
@@ -56,14 +62,12 @@ else
 ##
 ######################################################
 
-// Set to TRUE for testing
-$useTestEmail = FALSE;
-
-if($useTestEmail == TRUE)
+else
 	{
 
-		$postedEmail = '{"headers":{"from example@example.com  sat nov 20 22":"02:41 2010","return-path":"<example@example.com>","x-original-to":"mail@post.demo.mailnuggets.com","delivered-to":"mail@post.demo.mailnuggets.com","received":["from mail-wy0-f181.google.com (mail-wy0-f181.google.com [74.125.82.181]) by mail.mailnuggets.com (Postfix) with ESMTP id 21C3BE992 for <mail@post.demo.mailnuggets.com>; Sat, 20 Nov 2010 22:02:40 -0500 (EST)","by wyb36 with SMTP id 36so1316109wyb.40 for <mail@post.demo.mailnuggets.com>; Sat, 20 Nov 2010 19:02:40 -0800 (PST)","by 10.216.141.79 with SMTP id f57mr3446869wej.101.1290308560097; Sat, 20 Nov 2010 19:02:40 -0800 (PST)","by 10.216.86.129 with HTTP; Sat, 20 Nov 2010 19:02:40 -0800 (PST)"],"dkim-signature":"v=1; a=rsa-sha256; c=relaxed\/relaxed; d=gmail.com; s=gamma; h=domainkey-signature:mime-version:received:received:date:message-id :subject:from:to:content-type; bh=AJI14WuSViQ1Tn2HM8g83+mUumXYHUQie+kWqS2gc0A=; b=KjRM8LkON8LOms5luodtNlKWxGRisiTPU\/UoT7elsijQtGD61qymZin8Wmwt+gUyHq gxc+0WEOxZQdzjCLXz55gUe4kfV4hrvs6cjnDiRVtVNxBSR+eVXnLEwcsOh9B\/3DZ1rj 4ZKKMC1NEENjXLor+G\/FgAxPpioXDxMf1MSso=","domainkey-signature":"a=rsa-sha1; c=nofws; d=gmail.com; s=gamma; h=mime-version:date:message-id:subject:from:to:content-type; b=njXZSFk0ajP3vJU0gFzS4K8eXHFRo0Nb03IJniQ1fSjg+UKFOoJujE7Mfhgx\/TsJSu 1pd3j1ZeauW6BllKHws\/hcdWx9LvNHLiYubGdtWBvxX30lI2\/RGYUV+PRXIH3wfQ+01K m8H+PnJfFFzuSRZMHO\/6SaCmd1Pw2zW+RDEwQ=","mime-version":"1.0","date":"Sun, 21 Nov 2010 11:02:40 +0800","message-id":"<AANLkTik-UGO5D1mhiFJrpHBEqgNer3pRsnx=NghMG9Sj@mail.gmail.com>","subject":"Test email subject","from":"Tom Jones <example@example.com>","to":"mail@post.demo.mailnuggets.com","content-type":"multipart\/alternative; boundary=00504502b1bd6011f10495875f8d"},"ctype_primary":"multipart","ctype_secondary":"alternative","ctype_parameters":{"boundary":"00504502b1bd6011f10495875f8d"},"parts":[{"headers":{"content-type":"text\/plain; charset=ISO-8859-1"},"ctype_primary":"text","ctype_secondary":"plain","ctype_parameters":{"charset":"ISO-8859-1"},"body":"Test email message\n\n"},{"headers":{"content-type":"text\/html; charset=ISO-8859-1"},"ctype_primary":"text","ctype_secondary":"html","ctype_parameters":{"charset":"ISO-8859-1"},"body":"<b>Test email message<\/b><br>\n\n"}]}';
+		$postedEmail = '{"headers":{"from jwooten37830@icloud.com  sun aug  7 14":"26:40 2022","return-path":"<jwooten37830@icloud.com>","x-original-to":"mail@leads.woo37830.mailnuggets.com","delivered-to":"mail@leads.woo37830.mailnuggets.com","received":["from st43p00im-ztfb10063301.me.com (st43p00im-ztfb10063301.me.com [17.58.63.179]) by mail.mailnuggets.com (Postfix) with ESMTP id 2BD4641817 for <mail@leads.woo37830.mailnuggets.com>; Sun,  7 Aug 2022 14:26:40 +0000 (UTC)","from [10.0.0.197] (st43p00im-dlb-asmtp-mailmevip.me.com [17.42.251.41]) by st43p00im-ztfb10063301.me.com (Postfix) with ESMTPSA id 88C287001C3 for <mail@leads.woo37830.mailnuggets.com>; Sun,  7 Aug 2022 14:26:39 +0000 (UTC)"],"dkim-signature":"v=1; a=rsa-sha256; c=relaxed\/relaxed; d=icloud.com; s=1a1hai; t=1659882399; bh=VdcXYmUWZl7h4MHOj0tMy\/GditxPcxbsn676Qmw77RY=; h=From:Content-Type:Mime-Version:Subject:Message-Id:Date:To; b=02R034dtY7hZCBiKlD9fVINNn78nlelZ6XnD93oBB5IBtDfw4zL2IJgGHS5RAX1F3 tzNQIY+HJ4nP++is9FhWzbOYGQq5Ydd8rVGUd1ydla2jl1CRh6lpqsEM7I+S9HLo4Y iC8wXY8uhEolwTU9mlC0gVdL0l0AZarYdM9rWdIp4185dAznn4grzTPDfMxki2edTo wbpzpdRDoLmzfh7tEg91\/KWsyOFzlY3j1WmKM5s7kuYh0rF1GcRwsnpD1sU0Ny+Zx\/ +onj7CcvWMVdrl9uumYmLpVfcMBrr2nojdDnkyXWZuVLeZl1T+rLuqpQI8CdRixyS7 NO5t5mCuCCl1w==","from":"John Wooten <jwooten37830@icloud.com>","content-type":"multipart\/alternative; boundary=\"Apple-Mail=_E09D50C6-BF8A-4AF2-8D66-7540EACC5B06\"","mime-version":"1.0 (Mac OS X Mail 13.4 \\(3608.120.23.2.7\\))","subject":"Test9","message-id":"<B58E7329-71D8-4628-B994-E3FE018AAFC5@icloud.com>","date":"Sun, 7 Aug 2022 10:26:38 -0400","to":"mail@leads.woo37830.mailnuggets.com","x-mailer":"Apple Mail (2.3608.120.23.2.7)","x-proofpoint-guid":"1DE6kmJ46h0ZWwGF2P0WskX9wakfX0TT","x-proofpoint-orig-guid":"1DE6kmJ46h0ZWwGF2P0WskX9wakfX0TT","x-proofpoint-virus-version":"vendor=fsecure engine=1.1.170-22c6f66c430a71ce266a39bfe25bc2903e8d5c8f:6.0.138,18.0.883,17.11.64.514.0000000 definitions=2022-06-21_08:2020-02-14_02,2022-06-21_08,2022-02-23_01 signatures=0","x-proofpoint-spam-details":"rule=notspam policy=default score=0 mlxlogscore=234 mlxscore=0 clxscore=1015 suspectscore=0 bulkscore=0 adultscore=0 phishscore=0 malwarescore=0 spamscore=0 classifier=spam adjust=0 reason=mlx scancount=1 engine=8.12.0-2206140000 definitions=main-2208070078"},"ctype_primary":"multipart","ctype_secondary":"alternative","ctype_parameters":{"boundary":"Apple-Mail=_E09D50C6-BF8A-4AF2-8D66-7540EACC5B06"},"parts":[{"headers":{"content-transfer-encoding":"quoted-printable","content-type":"text\/plain; charset=us-ascii"},"ctype_primary":"text","ctype_secondary":"plain","ctype_parameters":{"charset":"us-ascii"},"body":"Test that (865) 300-4774 for wooten.666@gmail.com =\n<mailto:wooten.666@gmail.com> John Wooten 106 Crestview Lane Oak Ridge =\nTN, 37830 gets parsed.\n\n\n"},{"headers":{"content-transfer-encoding":"7bit","content-type":"text\/html; charset=us-ascii"},"ctype_primary":"text","ctype_secondary":"html","ctype_parameters":{"charset":"us-ascii"},"body":"<html><head><meta http-equiv=\"Content-Type\" content=\"text\/html; charset=us-ascii\"><\/head><body style=\"word-wrap: break-word; -webkit-nbsp-mode: space; line-break: after-white-space;\" class=\"\">Test that (865) 300-4774 for&nbsp;<a href=\"mailto:wooten.666@gmail.com\" class=\"\">wooten.666@gmail.com<\/a>&nbsp;John Wooten 106 Crestview Lane Oak Ridge TN, 37830 gets parsed.<div class=\"\"><br class=\"\"><\/div><\/body><\/html>\n"}]}';
 
+$data = '{"Coords":[{"Accuracy":"65","Latitude":"53.277720488429026","Longitude":"-9.012038778269686","Timestamp":"Fri Jul 05 2013 11:59:34 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.277720488429026","Longitude":"-9.012038778269686","Timestamp":"Fri Jul 05 2013 11:59:34 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.27770755361785","Longitude":"-9.011979642121824","Timestamp":"Fri Jul 05 2013 12:02:09 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.27769091555766","Longitude":"-9.012051410095722","Timestamp":"Fri Jul 05 2013 12:02:17 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.27769091555766","Longitude":"-9.012051410095722","Timestamp":"Fri Jul 05 2013 12:02:17 GMT+0100 (IST)"}]}';
 	}
 
 
@@ -74,7 +78,7 @@ if(!(function_exists('json_decode')))
 		$error = 1;
 	}
 
-if(count($_POST) < 1)
+if(count($_POST) < 1 && !$useTestEmail)
 	{
 		echo " ERROR: No data posted. ";
 		$error = 1;
@@ -82,6 +86,7 @@ if(count($_POST) < 1)
 
 if(function_exists('json_decode') && $postedEmail != null)
 	{
+		$emailObject = json_decode($postedEmail);
 		if(!($emailObject == json_decode($postedEmail)))
 			{
 				// the string is not valid JSON
@@ -95,19 +100,18 @@ function objectToArray( $object )
  	{
      	if( !is_object( $object ) && !is_array( $object ) )
 		    {
+					echo "return non-object";
 		         return $object;
 		    }
 
 		if( is_object( $object ) )
 		    {
 		         $object = get_object_vars( $object );
+						 echo "return object";
 		    }
-
+				echo "return array_map of objectToArray of object";
      	return array_map( 'objectToArray', $object );
 	}
-
-// convert object to an array recursively
-$emailArray = objectToArray($emailObject);
 
 
 function decodeQuotedPrintable ($message)
@@ -121,17 +125,59 @@ function decodeQuotedPrintable ($message)
 		return $message;
 	}
 
+	// Define recursive function to extract nested values
+	function printValues($arr) {
+	    global $count;
+	    global $values;
+
+	    // Check input is an array
+	    if(!is_array($arr)){
+				echo "Type of arr is: ".getType($arr)."\n";
+	        die("ERROR: Input is not an array");
+	    }
+
+	    /*
+	    Loop through array, if value is itself an array recursively call the
+	    function else add the value found to the output items array,
+	    and increment counter by 1 for each value found
+	    */
+	    foreach($arr as $key=>$value){
+	        if(is_array($value)){
+	            printValues($value);
+	        } else{
+	            $values[] = $value;
+	            $count++;
+	        }
+	    }
+
+	    // Return total count and values found in array
+	    return array('total' => $count, 'values' => $values);
+	}
 // get key variables from $emailArray object if no errors
 if($error != 1)
 	{
+		// convert object to an array recursively
+		  echo "postedEmail is: ".getType($postedEmail)."\n";
 
-		$subject = $emailArray['headers']['subject'];
+		$emailObject = json_decode( $postedEmail, true ); // emailObject is JSON object
+		if( $emailObject == null )
+		{
+			die("ERROR: emailObject is null");
+		}
+		echo "emailObject is: ".getType($emailObject)."\n";
+		print_r($emailObject);
+		$emailArray = $emailObject;
+//		$subject = $emailArray['headers']['subject'];
 
 		if($emailArray['body'] != null)
 			{
 				$message = $emailArray['body'];
 			}
+			else
+			{
+				die("ERROR: email array does not contain a body");
 
+			}
 		// if messages comes as both plain and html parts (multipart) treat the plain one as the main one
 		if($emailArray['body'] == null && substr_count($emailArray['parts']['0']['headers']['content-type'], 'plain') > 0)
 			{
@@ -388,7 +434,7 @@ $log = "Email post log:\n$email \n";
 echo "Email error=$error";
 if( $error !== 1 )
 {
-	$prospect = new Prospect($message);
+	$prospect = new Prospect($from, $message);
 	echo "\n---------------------Prospect-----------------\n";
 	echo $prospect;
 	echo "\n----------------------------------------------\n";
